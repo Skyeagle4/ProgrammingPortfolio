d03b183dab09a6147babeb5405cf52b18e171ca3
Keegan Hardy <keegan780@gmail.com>
2017-05-24 16:08:11 -0600

    Update pigLatin.cpp

--- ProgrammingFolder/Language Migration/pigLatin.cpp
+++ ProgrammingFolder/Language Migration/pigLatin.cpp
@@ -1,18 +1,45 @@
+/*Keegan Hardy • Pig Latin Translator • February 2017*/
+
 #include <iostream>
-#include <cstring>
-#include <ctype.h>
-#include <string.h>
+#include <string>
 
 using namespace std;
 
+string pigLatin(string);
+
 int main() {
-    char a [50];
-    char b [50];
-    char c [50];
-    char d [50];
-    char e [50];
-    cout << "Please enter a phrase: ";
-    cin >> a >> b >> c >> d >> e;
-    
-    cout << "Translation to Pig Latin: " << a << " " << b << " " << c << " " << d << " " << e << endl;
+   string translate;
+   
+   cout << "Welcome to the Pig Latin Translator! Please enter the sentence that you would\nlike to have translated: " << endl;
+   getline(cin, translate);
+   translate = pigLatin(translate);
+   cout << translate << endl;
+   
+   return 0;
+}
+
+string pigLatin(string input) {
+   string translation, sentence = "";
+   int l = 0, i = 0;
+   
+   while (input[i] != '\0') {
+      if (input.find(' ', i) != -1) {
+         l = input.find(' ', i);
+         l -= i;
+         translation = input.substr(i, l);
+         translation.insert(l, "ay");
+         translation.insert(l, 1, input[i]);
+         translation.erase(0, 1);
+         i += l +1;
+      } else {
+         translation = input.substr(i);
+         l = translation.length();
+         translation.insert(l, "ay");
+         translation.insert(l, 1, input[i]);
+         translation.erase(0, 1);
+         i = input.length();
+      }
+      sentence += (translation + " ");
+   }
+   return sentence;
 }
